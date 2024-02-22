97d37361caac7e8b9709e408c9a8c819c6371f70
gis2010 <1070073+gis2010@users.noreply.github.com>
2024-02-22 15:03:00 -0500

    Update listen_pipeline_schedule_load.go

--- app/mainapp/scheduler/listen_pipeline_schedule_load.go
+++ app/mainapp/scheduler/listen_pipeline_schedule_load.go
@@ -3,10 +3,10 @@ package scheduler
 import (
 	"log"
 
-	dpconfig "github.com/dataplane-app/dataplane/app/mainapp/config"
-	"github.com/dataplane-app/dataplane/app/mainapp/database"
-	"github.com/dataplane-app/dataplane/app/mainapp/database/models"
-	"github.com/dataplane-app/dataplane/app/mainapp/messageq"
+	dpconfig "github.com/enteprise/etl-central/app/mainapp/config"
+	"github.com/enteprise/etl-central/app/mainapp/database"
+	"github.com/enteprise/etl-central/app/mainapp/database/models"
+	"github.com/enteprise/etl-central/app/mainapp/messageq"
 
 	"github.com/go-co-op/gocron"
 	"gorm.io/gorm"
